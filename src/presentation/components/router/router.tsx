ca4dba331627555deea21a3cc64aeab357a0b46d
Gabriel Resende <gabriel.resende06@gmail.com>
2021-01-30 19:43:40 +0100

    fix: move global styles to index

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { Login } from '@/presentation/pages'
 
-import '../../styles/global.scss'
-
 const Router: React.FC = () => {
   return (
     <BrowserRouter>
