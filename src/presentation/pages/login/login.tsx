ba199b3cb8a5d1417c47545c215dc3fe1b656957
Gabriel Resende <gabriel.resende06@gmail.com>
2021-01-31 09:04:43 +0100

    feat: ensure Login will not load spinner and error on start

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,24 +1,43 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import Styles from './login-styles.scss'
 import { Footer, FormStatus, Input, LoginHeader as Header } from '@/presentation/components'
+import Context from '@/presentation/contexts/form/form-context'
+
+type State = {
+  isLoading: boolean
+  errorMessage: string
+}
+
+type Action = { type: string }
+
+const reducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
 
 const Login: React.FC = () => {
+  const [state] = useReducer(reducer, { isLoading: false, errorMessage: '' })
+
   return (
     <div className={Styles.login}>
         <Header />
-        <form className={Styles.form}>
-            <h2>Login</h2>
+        <Context.Provider value={state}>
+            <form className={Styles.form}>
+                <h2>Login</h2>
 
-            <Input type="email" name="email" placeholder="Digite seu e-mail" />
-            <Input type="password" name="password" placeholder="Digite sua senha" />
+                <Input type="email" name="email" placeholder="Digite seu e-mail" />
+                <Input type="password" name="password" placeholder="Digite sua senha" />
 
-            <button className={Styles.submit} type="submit">Entrar</button>
-            <span className={Styles.link}>
-                Criar Conta
-            </span>
+                <button className={Styles.submit} type="submit">Entrar</button>
+                <span className={Styles.link}>
+                    Criar Conta
+                </span>
 
-            <FormStatus />
-        </form>
+                <FormStatus />
+            </form>
+        </Context.Provider>
         <Footer />
     </div>
   )
