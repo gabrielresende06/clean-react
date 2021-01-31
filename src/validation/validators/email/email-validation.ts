f0f14e45b0fa0fe214bdcfac3847ed9c3484b96d
Gabriel Resende <gabriel.resende06@gmail.com>
2021-01-31 20:05:27 +0100

    feat: ensure ValidationComposite returns error if any validation fails

--- src/validation/validators/email/email-validation.ts
+++ src/validation/validators/email/email-validation.ts
@@ -4,8 +4,8 @@ import { InvalidFieldError } from '@/validation/errors'
 export class EmailValidation implements FieldValidation {
   constructor (readonly field: string) {}
 
-  validate (value: string): Error | boolean {
+  validate (value: string): Error {
     const emailRegex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
-    return (!value || emailRegex.test(value)) ? false : new InvalidFieldError()
+    return (!value || emailRegex.test(value)) ? null : new InvalidFieldError()
   }
 }
--- src/validation/validators/min-length/min-length-validation.ts
+++ src/validation/validators/min-length/min-length-validation.ts
@@ -4,7 +4,7 @@ import { InvalidFieldError } from '@/validation/errors'
 export class MinLengthValidation implements FieldValidation {
   constructor (readonly field: string, private readonly minLength: number) {}
 
-  validate (value: string): Error | boolean {
-    return value.length >= this.minLength ? false : new InvalidFieldError()
+  validate (value: string): Error {
+    return value.length >= this.minLength ? null : new InvalidFieldError()
   }
 }
