c2b49af50af5bb94f898bbeac52ab2cb7d047224
Gabriel Resende <gabriel.resende06@gmail.com>
2021-01-31 10:38:26 +0100

    test: ensure Login calls validation with correct email

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,16 +1,30 @@
 import React from 'react'
 import '@testing-library/jest-dom'
-import { render, RenderResult, screen } from '@testing-library/react'
+import { render, RenderResult, screen, fireEvent } from '@testing-library/react'
 import { Login } from '@/presentation/pages'
+import { Validation } from '@/presentation/protocols/validation'
 
 type SutTypes = {
   sut: RenderResult
+  validationSpy: ValidationSpy
+}
+
+class ValidationSpy implements Validation {
+  errorMessage: string
+  input: object
+
+  validate (input: object): string {
+    this.input = input
+    return this.errorMessage
+  }
 }
 
 const makeSut = (): SutTypes => {
-  const sut = render(<Login />)
+  const validationSpy = new ValidationSpy()
+  const sut = render(<Login validation={validationSpy} />)
   return {
-    sut
+    sut,
+    validationSpy
   }
 }
 
@@ -31,5 +45,15 @@ describe('Login Component', () => {
     const passwordStatus = screen.getByTestId('password-status')
     expect(passwordStatus).toHaveProperty('title', 'Campo obrigatório')
     expect(passwordStatus).toHaveTextContent('🔴')
+  })
+
+  test('Should call Validation with correct email', () => {
+    const { validationSpy } = makeSut()
+
+    const emailInput = screen.getByTestId('email')
+    fireEvent.input(emailInput, { target: { value: 'any_email' } })
+    expect(validationSpy.input).toEqual({
+      email: 'any_email'
+    })
   })
 })
