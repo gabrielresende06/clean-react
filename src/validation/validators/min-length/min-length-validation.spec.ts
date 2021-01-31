40cd002edaf5868e9f49d1461d9e61b1f08c8831
Gabriel Resende <gabriel.resende06@gmail.com>
2021-01-31 19:36:31 +0100

    refactor: change the hard code values to get from faker

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -7,13 +7,13 @@ const makeSut = (minLength: number = 5): MinLengthValidation => new MinLengthVal
 describe('MinLengthValidation', () => {
   test('Should return error if value is invalid', () => {
     const sut = makeSut()
-    const error = sut.validate('123')
+    const error = sut.validate(faker.random.alphaNumeric(4))
     expect(error).toEqual(new InvalidFieldError())
   })
 
   test('Should return false if value is valid', () => {
     const sut = makeSut()
-    const error = sut.validate('12345')
+    const error = sut.validate(faker.random.alphaNumeric(5))
     expect(error).toBeFalsy()
   })
 })
