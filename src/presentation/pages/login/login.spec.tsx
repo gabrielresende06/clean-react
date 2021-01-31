0f2b5f0d26b764310c12b2111b15e0d2825c9bbd
Gabriel Resende <gabriel.resende06@gmail.com>
2021-01-31 10:44:45 +0100

    test: ensure Login calls validation with correct password

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -56,4 +56,14 @@ describe('Login Component', () => {
       email: 'any_email'
     })
   })
+
+  test('Should call Validation with correct password', () => {
+    const { validationSpy } = makeSut()
+
+    const passwordInput = screen.getByTestId('password')
+    fireEvent.input(passwordInput, { target: { value: 'any_password' } })
+    expect(validationSpy.input).toEqual({
+      password: 'any_password'
+    })
+  })
 })
