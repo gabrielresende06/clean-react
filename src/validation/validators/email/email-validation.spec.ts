f7cb1a2eac2d9ee739023efe255464d8cfdb2f12
Gabriel Resende <gabriel.resende06@gmail.com>
2021-01-31 19:17:29 +0100

    test: ensure EmailValidation returns error fie email is invalid

---
+++ src/validation/validators/email/email-validation.spec.ts
@@ -0,0 +1,13 @@
+import { EmailValidation } from '@/validation/validators'
+import { InvalidFieldError } from '@/validation/errors'
+import * as faker from 'faker'
+
+const makeSut = (): EmailValidation => new EmailValidation(faker.database.column())
+
+describe('EmailValidation', () => {
+  test('Should return error if email is invalid', () => {
+    const sut = makeSut()
+    const error = sut.validate('')
+    expect(error).toEqual(new InvalidFieldError())
+  })
+})
