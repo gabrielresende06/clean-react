5250f4100385b20ab8a77e785cd145508e3704a1
Gabriel Resende <gabriel.resende06@gmail.com>
2021-02-05 23:14:41 +0100

    test: ensure Signup does not call submit if form is invalid

--- src/main/test/cypress/integration/signup.spec.ts
+++ src/main/test/cypress/integration/signup.spec.ts
@@ -106,4 +106,11 @@ describe('SignUp', () => {
     cy.getByTestId('submit').dblclick()
     cy.get('@request.all').should('have.length', 1)
   })
+
+  it('should not call submit if form is invalid', () => {
+    Http.mockOk()
+
+    cy.getByTestId('email').focus().type(faker.internet.email()).type('{enter}')
+    cy.get('@request.all').should('have.length', 0)
+  })
 })
