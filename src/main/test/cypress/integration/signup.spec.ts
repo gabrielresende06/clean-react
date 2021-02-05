dcce16692ef9bd9dbffc4e0ae9ef0816640d4e55
Gabriel Resende <gabriel.resende06@gmail.com>
2021-02-05 23:08:10 +0100

    test: ensure Signup present correct error EmailInUseError on 403

--- src/main/test/cypress/integration/signup.spec.ts
+++ src/main/test/cypress/integration/signup.spec.ts
@@ -1,5 +1,17 @@
-import { testInputStatus } from '../support/form-helper'
+import { testInputStatus, testMainError } from '../support/form-helper'
 import faker from 'faker'
+import * as Http from '../support/signup-mocks'
+
+const baseUrl: string = Cypress.config().baseUrl
+
+const simulateValidSubmit = (): void => {
+  cy.getByTestId('name').focus().type(faker.name.findName())
+  cy.getByTestId('email').focus().type(faker.internet.email())
+  const password = faker.internet.password(5)
+  cy.getByTestId('password').focus().type(password)
+  cy.getByTestId('passwordConfirmation').focus().type(password)
+  cy.getByTestId('submit').click()
+}
 
 describe('SignUp', () => {
   beforeEach(() => {
@@ -52,5 +64,12 @@ describe('SignUp', () => {
 
     cy.getByTestId('submit').should('not.have.attr', 'disabled')
     cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
+
+  it('should present EmailInUseError on 403', () => {
+    Http.mockEmailInUseError()
+    simulateValidSubmit()
+    testMainError('Esse e-mail já está em uso')
+    cy.url().should('eq', `${baseUrl}/signup`)
   })
 })
