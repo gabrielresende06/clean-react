296f6889e3a9d9660f06aa2ddad731bc82267432
Gabriel Resende <gabriel.resende06@gmail.com>
2021-02-05 12:36:38 +0100

    refactor: add input animations

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useRef } from 'react'
 import Styles from './input-styles.scss'
 import Context from '@/presentation/contexts/form/form-context'
 
@@ -7,28 +7,22 @@ type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>
 const Input: React.FC<Props> = (props: Props) => {
   const { state, dispatch } = useContext(Context)
   const error = state.errors[`${props.name}`]
-
-  const enableInput = (event: React.FocusEvent<HTMLInputElement>): void => {
-    event.target.readOnly = false
-  }
-
-  const handleChange = (event: React.FocusEvent<HTMLInputElement>): void => {
-    dispatch({ type: event.target.name, value: event.target.value })
-  }
-
-  const getStatus = (): string => {
-    return error ? '🔴' : '🟢'
-  }
-
-  const getTitle = (): string => {
-    return error || 'Tudo certo!'
-  }
+  const inputRef = useRef<HTMLInputElement>()
 
   return (
       <div className={Styles.inputWrap}>
-        <input {...props} data-testid={`${props.name}`} autoComplete="off" readOnly onFocus={enableInput} onChange={handleChange} />
-        <span data-testid={`${props.name}-status`} title={getTitle()} className={Styles.status}>
-            {getStatus()}
+        <input {...props}
+               ref={inputRef}
+               placeholder=" "
+               data-testid={`${props.name}`}
+               autoComplete="off"
+               readOnly onFocus={event => { event.target.readOnly = false }}
+               onChange={event => { dispatch({ type: event.target.name, value: event.target.value }) }} />
+        <label onClick={() => { inputRef.current.focus() }}>{props.placeholder}</label>
+        <span data-testid={`${props.name}-status`}
+              title={error || 'Tudo certo!'}
+              className={Styles.status}>
+            {error ? '🔴' : '🟢'}
         </span>
       </div>
   )
