477b4111c600e914c12d38a772e8a15befc3c83d
Gabriel Resende <gabriel.resende06@gmail.com>
2021-02-08 14:15:00 +0100

    test: ensure SurveyItem Component renders with correct values

---
+++ src/presentation/pages/survey-list/components/survey-item/survey-item.spec.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import { SurveyItem } from '@/presentation/pages/survey-list/components'
+import { mockSurveyModel } from '@/domain/test'
+import { SurveyModel } from '@/domain/models'
+import { IconName } from '@/presentation/components'
+
+const makeSut = (survey: SurveyModel): void => {
+  render(<SurveyItem survey={survey} />)
+}
+
+describe('SurveyItem Component', () => {
+  test('Should render with correct values', () => {
+    const survey = mockSurveyModel()
+    survey.didAnswer = true
+    survey.date = new Date('2020-01-10T00:00:00')
+    makeSut(survey)
+    expect(screen.getByTestId('icon')).toHaveAttribute('src', IconName.thumbUp)
+    expect(screen.getByTestId('question')).toHaveTextContent(survey.question)
+    expect(screen.getByTestId('day')).toHaveTextContent('10')
+    expect(screen.getByTestId('month')).toHaveTextContent('jan')
+    expect(screen.getByTestId('year')).toHaveTextContent('2020')
+  })
+})
