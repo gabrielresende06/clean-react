fda686b6c84b1aa2313c33a3c880d0a10d44fff4
Gabriel Resende <gabriel.resende06@gmail.com>
2021-02-09 12:05:51 +0100

    test: ensure SurveyList present error on UnexpectedError

---
+++ src/main/test/cypress/integration/survey-list.spec.ts
@@ -0,0 +1,18 @@
+import * as Helper from '../support/helpers'
+import * as Http from '../support/survey-list-mocks'
+import * as faker from 'faker'
+
+describe('SurveyList', () => {
+  beforeEach(() => {
+    Helper.setLocalStorageItem('account', {
+      accessToken: faker.random.uuid(),
+      name: faker.name.findName()
+    })
+  })
+
+  it('should present error on UnexpectedError', () => {
+    Http.mockUnexpectedError()
+    cy.visit('')
+    cy.getByTestId('error').should('contain.text', 'Algo de errado aconteceu. Tente novamente em breve.')
+  })
+})
