fc0cb5f0b16286fddb624a2abfc3f4d14989255c
Gabriel Resende <gabriel.resende06@gmail.com>
2021-03-11 11:53:12 +0100

    test: ensure RemoteLoadSurveyResult throws the right exceptions

---
+++ src/data/usecases/load-survey-result/remote-load-survey-result.spec.ts
@@ -0,0 +1,47 @@
+import { RemoteLoadSurveyResult } from '@/data/usecases/load-survey-result/remote-load-survey-result'
+import * as faker from 'faker'
+import { HttpGetClientSpy } from '@/data/test'
+import { HttpStatusCode } from '@/data/protocols/http'
+import { AccessDeniedError, UnexpectedError } from '@/domain/errors'
+
+type SutTypes = {
+  sut: RemoteLoadSurveyResult
+  httpGetClientSpy: HttpGetClientSpy
+}
+
+const makeSut = (url: string = faker.internet.url()): SutTypes => {
+  const httpGetClientSpy = new HttpGetClientSpy()
+  const sut = new RemoteLoadSurveyResult(url, httpGetClientSpy)
+
+  return {
+    sut,
+    httpGetClientSpy
+  }
+}
+
+describe('RemoteLoadSurveyResult', () => {
+  test('Should call HttpGetClient with correct URL', async () => {
+    const url = faker.internet.url()
+    const { sut, httpGetClientSpy } = makeSut(url)
+    await sut.load()
+    expect(httpGetClientSpy.url).toBe(url)
+  })
+
+  test('Should throw AccessDeniedError if HttpGetClient returns 403', async () => {
+    const { sut, httpGetClientSpy } = makeSut()
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.forbidden
+    }
+    const promise = sut.load()
+    await expect(promise).rejects.toThrow(new AccessDeniedError())
+  })
+
+  test('Should throw UnexpectedError if HttpGetClient returns 404', async () => {
+    const { sut, httpGetClientSpy } = makeSut()
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.notFound
+    }
+    const promise = sut.load()
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
+})
