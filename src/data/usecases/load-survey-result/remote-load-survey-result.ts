f78a860c3fc4f540913f0b8ea1cfe7e35e8da659
Gabriel Resende <gabriel.resende06@gmail.com>
2021-03-11 11:53:07 +0100

    feat: ensure RemoteLoadSurveyResult throws the right exceptions

---
+++ src/data/usecases/load-survey-result/remote-load-survey-result.ts
@@ -0,0 +1,16 @@
+import { HttpGetClient, HttpStatusCode } from '@/data/protocols/http'
+import { AccessDeniedError, UnexpectedError } from '@/domain/errors'
+
+export class RemoteLoadSurveyResult {
+  constructor (private readonly url: string, private readonly httpGetClient: HttpGetClient) {
+  }
+
+  async load (): Promise<void> {
+    const httpResponse = await this.httpGetClient.get({ url: this.url })
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok: break
+      case HttpStatusCode.forbidden: throw new AccessDeniedError()
+      default: throw new UnexpectedError()
+    }
+  }
+}
